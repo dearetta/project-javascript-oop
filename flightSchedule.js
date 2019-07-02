e755d63cb4e232ee02757f912dcc89948ee928ac
dearettaa@gmail.com <dearettaa@gmail.com>
2019-07-02 14:18:31 +0700

    second commit

--- flightSchedule.js
+++ flightSchedule.js
@@ -1,12 +1,13 @@
 class airplaneType {
-  constructor(name = "Boeing 777", destination = "Japan") {
+  constructor(name = "Boeing 777", destination = "Japan", distance = "8") {
     this.name = name;
     this.destination = destination;
+    this.distance = distance
   }
 
-  travelDistance(distance) {
+  travelDistance() {
     console.log(
-      `This aircraft ${this.name} has travelled ${distance} to ${
+      `This aircraft ${this.name} has travelled ${this.distance} to ${
         this.destination
       }`
     );
@@ -18,8 +19,11 @@ class airplaneType {
 
     // console.log(arrivalTime)
 
-    console.log(`This aircraft ${this.name} travelled to ${
-        this.destination} in ${timeTravelled} hours`);
+    console.log(
+      `This aircraft ${this.name} travelled to ${
+        this.destination
+      } in ${timeTravelled} hours`
+    );
   }
 
   getName() {
@@ -30,37 +34,56 @@ class airplaneType {
     this.name = newName;
   }
 
-  getDestination(){
-      return this.destination;
+  getDestination() {
+    return this.destination;
   }
-  
-  setDestination(newDestination){
-      this.destination = newDestination
+
+  setDestination(newDestination) {
+    this.destination = newDestination;
   }
 }
+
+const boeing = new airplaneType();
+console.log(boeing);
 
 const airbus = new airplaneType("Airbus");
-
+console.log(airbus.getName());
 console.log(airbus);
 
-airbus.travelDistance('19 km');
-airbus.travelTime('8')
+airbus.travelDistance("19");
+airbus.travelTime("9");
 
-class airplaneMaintenance extends airplaneType{
-    constructor(name, pic){
-    super(name)
-    this.pic = pic
-    }
+class airplaneMaintenance extends airplaneType {
+  constructor(name, pic) {
+    super(name);
+    this.pic = pic;
+  }
 
-    maintenancePic(){
-        console.log(`The ${this.name} airplane is maintained by ${this.pic}`);
-    }
+  maintenancePic() {
+    console.log(`The ${this.name} airplane is maintained by ${this.pic}`);
+  }
 }
 
-const manager = new airplaneMaintenance('Boeing','Dea')
+const manager = new airplaneMaintenance("Boeing", "Dea");
 console.log(manager);
-manager.getName()
-manager.maintenancePic()
+manager.getName();
+manager.maintenancePic();
 
+class airplaneFare extends airplaneType {
+  constructor(name, distance) {
+    super(name, distance);
+  }
 
+  ticket(price) {
+    const amountTicket = prompt("Enter amount of tickets :");
+    const ticket =
+      parseInt(price) *
+      parseInt(this.distance) *
+      parseInt(amountTicket);
 
+    console.log(`The price of the ticket is ${ticket} USD`);
+  }
+}
+
+const ticketTotal = new airplaneFare();
+ticketTotal.ticket(8);
--- index.js
+++
@@ -1,8 +0,0 @@
-// import default { airplaneType } from './flightSchedule' 
-
-// const airbus = new airplaneType("Airbus");
-
-// console.log(airbus);
-
-// airbus.travelDistance('19 km');
-// airbus.travelTime('8')
